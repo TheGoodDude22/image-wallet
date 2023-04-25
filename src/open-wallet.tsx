1bc9d6c792c3cb65c9d69e00b3e168f8125d1e8d
TheGoodDude22 <61601241+TheGoodDude22@users.noreply.github.com>
2023-04-25 10:20:04 -0500

    progress

--- src/open-wallet.tsx
+++ src/open-wallet.tsx
@@ -4,7 +4,7 @@ import { usePromise } from "@raycast/utils";
 import { ReactNode } from "react";
 
 import { walletPath, fetchFiles } from "./utils";
-import { Card } from "./types";
+import { Card, Pocket } from "./types";
 
 export default function Command() {
 	const { isLoading, data } = usePromise(loadGridComponents);
@@ -16,49 +16,76 @@ export default function Command() {
 			inset={Grid.Inset.Large}
 			navigationTitle="Image Wallet"
 			searchBarPlaceholder="Search Cards..."
+			searchBarAccessory={data?.dropdownNodes}
 			actions={
 				<ActionPanel>
-					{ loadEditActions() }
+					{ loadEditActionNodes() }
 				</ActionPanel>
 			  }
 		>
-			{ data }
+			{ data?.pocketNodes }
 		</Grid>
 	);
 }
 
 async function loadGridComponents() {
-	const outPockets:ReactNode[] = []
+	const pocketNodes:ReactNode[] = []
+	const dropdownNodes: ReactNode[] = []
 
 	fetchFiles(walletPath).then(pockets => {
-		pockets.forEach((pocket) => { outPockets.push(
-			<Grid.Section title={pocket.name} key={pocket.name || "unsorted"}>
-				{pocket.cards.map(card => (
-					<Grid.Item
-						key={card.path}
-						content={card.path}
-						title={card.name}
-						actions={loadCardActions(card)}
-					/>
-				))}
-			</Grid.Section>
-		)})
+		dropdownNodes.push(loadGridDropdownNodes(pockets))
+
+		pockets.forEach((pocket) => {
+			pocketNodes.push(loadPocketNodes(pocket));
+		})
 	})
 
-	return outPockets
+	return { pocketNodes, dropdownNodes }
 }
 
-function loadCardActions(item: Card) { return (
+function loadGridDropdownNodes(pockets: Pocket[]) { console.log(pockets); return (
+	<Grid.Dropdown tooltip="Pocket">
+		<Grid.Dropdown.Item
+			title="All Cards"
+			value="All Cards"
+		/>
+		<Grid.Dropdown.Section title="Pockets">
+			{pockets.filter(pocket => (pocket.name))
+				.map(pocket => (
+					<Grid.Dropdown.Item
+						title={pocket.name || "Unsorted"}
+						value={pocket.name || "Unsorted"}
+					/>
+				))
+			}
+		</Grid.Dropdown.Section>
+	</Grid.Dropdown>
+)}
+
+function loadPocketNodes(pocket:Pocket) { return(
+	<Grid.Section title={pocket.name} key={pocket.name || "unsorted"}>
+		{pocket.cards.map(card => (
+			<Grid.Item
+				key={card.path}
+				content={card.path}
+				title={card.name}
+				actions={loadCardActionNodes(card)}
+			/>
+		))}
+	</Grid.Section>
+)}
+
+function loadCardActionNodes(item: Card) { return (
 	<ActionPanel>
 		<ActionPanel.Section>
 			<Action.Paste content={{ file: item.path }} />
 			<Action.CopyToClipboard content={{ file: item.path }} />
 		</ActionPanel.Section>
-		{loadEditActions()}
+		{loadEditActionNodes()}
 	</ActionPanel>
 )}
 
-function loadEditActions() { return (
+function loadEditActionNodes() { return (
 	<ActionPanel.Section>
 		<Action.ShowInFinder
 			title="Open Wallet in Finder"
