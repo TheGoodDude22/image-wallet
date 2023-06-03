8645493ad9c4ce443a9f5b545666ac1c6d09ebec
ibll <61601241+ibll@users.noreply.github.com>
2023-06-03 02:55:20 -0500

    refactored, still not workign

--- src/utils.ts
+++ src/utils.ts
@@ -23,6 +23,7 @@ export async function fetchFiles(dir: string): Promise<Pocket[]> {
   loadPocketCards(dir).then((cards) => {
     if (cards.length > 0) pocketArr.push({ cards: cards });
   });
+
   const items = readdirSync(walletPath);
   items.forEach((item) => {
     const filePath = `${dir}/${item}`;
@@ -34,6 +35,14 @@ export async function fetchFiles(dir: string): Promise<Pocket[]> {
     if (fileName.startsWith(".")) return;
 
     loadPocketCards(`${dir}/${item}`).then((cards) => {
+      // const vids = cards.filter(item => {
+      //   return item.preview
+      // })
+
+      // console.log(vids)
+
+      if (item == "Videos") { console.log(cards) }
+
       pocketArr.push({ name: item, cards: cards });
     });
   });
@@ -54,79 +63,91 @@ async function loadPocketCards(dir: string): Promise<Card[]> {
     if (fileStats.isDirectory()) return;
     if (fileName.startsWith(".")) return;
 
-    const videoExts = [".mov", ".mp4"]
-    let previewPath: string | undefined = undefined
-    
+    const videoExts = [".mov", ".mp4"];
+
     if (videoExts.includes(fileExt)) {
-      const previewDir = `${environment.supportPath}/.previews`
+      const previewDir = `${environment.supportPath}/.previews`;
 
-      if (!existsSync(previewDir)){
+      if (!existsSync(previewDir)) {
         mkdirSync(previewDir);
+      }
+
+      const previewPath = await generateVideoPreview(
+        filePath,
+        `${previewDir}/${dir.replaceAll("/", "-")}:${item}.tiff`
+      );
+
+      // console.log(cardArr)
+
+      return cardArr.push({ name: fileName, path: filePath, preview: previewPath });
     }
 
-      const previewPathIn = await runJxa(`
-        ObjC.import("objc");
-        ObjC.import("CoreMedia");
-        ObjC.import("Foundation");
-        ObjC.import("AVFoundation");
-        ObjC.import("CoreGraphics");
-        ObjC.import("CoreImage");
-        ObjC.import("AppKit");
-        
-        const [inputPath, outputPath] = args;
-
-        // Load the video file
-        const assetURL = $.NSURL.fileURLWithPath(
-          inputPath
-        );
-
-        const asset = $.objc_getClass("AVAsset").assetWithURL(assetURL);
-        
-        // Ensure the video has a video track
-        if (asset.tracksWithMediaType($.AVMediaTypeVideo).count == 0) {
-          return "";
-        }
-
-        const frameCount = 15; // The number of frames to analyze
-        
-        // Set up the AVAssetReader for reading the video frames into pixel buffers
-        const reader = $.objc_getClass("AVAssetReader").alloc.initWithAssetError(
-          asset,
-          null
-        );
-        const track = asset.tracksWithMediaType($.AVMediaTypeVideo).objectAtIndex(0);
-        const settings = $.NSDictionary.dictionaryWithObjectForKey(
-          "420v",
-          "PixelFormatType"
-        );
-        readerOutput = $.objc_getClass(
-          "AVAssetReaderTrackOutput"
-        ).alloc.initWithTrackOutputSettings(track, settings);
-        reader.addOutput(readerOutput);
-        reader.startReading;
-        
-        // Read the video frames into pixel buffers
-        let buf = readerOutput.copyNextSampleBuffer;
-        if (reader.status != $.AVAssetReaderStatusFailed) {
-          const imageBufferRef = ObjC.castRefToObject(
-            $.CMSampleBufferGetImageBuffer(buf)
-          );
-        const CIImage = $.CIImage.imageWithCVPixelBuffer(imageBufferRef)
-        const imageRep = $.NSBitmapImageRep.alloc.initWithCIImage(CIImage)
-        const imageData = imageRep.TIFFRepresentation
-        imageData.writeToFileAtomically(outputPath, true)
-
-        return outputPath
-        }`,
-        [filePath, `${previewDir}/${dir.replaceAll("/", ":")}:${item}.tiff`]
-      )
-
-      previewPath = previewPathIn?.toString()
-    }
-
-    cardArr.push({ name: fileName, path: filePath, preview: previewPath });
-
+    return cardArr.push({ name: fileName, path: filePath });
   });
 
-  return cardArr
+  return cardArr;
+}
+
+async function generateVideoPreview(inputPath: string, outputPath: string): Promise<string | undefined> {
+  const previewPath = (
+    await runJxa(
+      `
+  ObjC.import("objc");
+  ObjC.import("CoreMedia");
+  ObjC.import("Foundation");
+  ObjC.import("AVFoundation");
+  ObjC.import("CoreGraphics");
+  ObjC.import("CoreImage");
+  ObjC.import("AppKit");
+  
+  const [inputPath, outputPath] = args;
+
+  // Load the video file
+  const assetURL = $.NSURL.fileURLWithPath(
+    inputPath
+  );
+
+  const asset = $.objc_getClass("AVAsset").assetWithURL(assetURL);
+  
+  // Ensure the video has a video track
+  if (asset.tracksWithMediaType($.AVMediaTypeVideo).count == 0) {
+    return undefined;
+  }
+
+  const frameCount = 15; // The number of frames to analyze
+  
+  // Set up the AVAssetReader for reading the video frames into pixel buffers
+  const reader = $.objc_getClass("AVAssetReader").alloc.initWithAssetError(
+    asset,
+    null
+  );
+  const track = asset.tracksWithMediaType($.AVMediaTypeVideo).objectAtIndex(0);
+  const settings = $.NSDictionary.dictionaryWithObjectForKey(
+    "420v",
+    "PixelFormatType"
+  );
+  readerOutput = $.objc_getClass(
+    "AVAssetReaderTrackOutput"
+  ).alloc.initWithTrackOutputSettings(track, settings);
+  reader.addOutput(readerOutput);
+  reader.startReading;
+  
+  // Read the video frames into pixel buffers
+  let buf = readerOutput.copyNextSampleBuffer;
+  if (reader.status != $.AVAssetReaderStatusFailed) {
+    const imageBufferRef = ObjC.castRefToObject(
+      $.CMSampleBufferGetImageBuffer(buf)
+    );
+  const CIImage = $.CIImage.imageWithCVPixelBuffer(imageBufferRef)
+  const imageRep = $.NSBitmapImageRep.alloc.initWithCIImage(CIImage)
+  const imageData = imageRep.TIFFRepresentation
+  imageData.writeToFileAtomically(outputPath, true)
+
+  return outputPath
+  }`,
+      [inputPath, outputPath]
+    )
+  )?.toString();
+
+  return previewPath;
 }
